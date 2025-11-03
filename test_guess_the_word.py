0304e578d4d64558a9b284040f0e07db3471eda1
Katia Gomez <kat2352417@maricopa.edu>
2025-11-02 17:45:49 -0700

    Built and tested main game function - ready to trasnfer to main file

--- test_guess_the_word.py
+++ test_guess_the_word.py
@@ -23,22 +23,55 @@ disney_movies = [
 
 
 def play_game():
-    random_movie = random.choice(disney_movies)
+    movie = random.choice(disney_movies)
+    random_movie = movie.lower()
     letter_list = list(random_movie)
+    word_length = len(random_movie)
+
     print("Guess the name of the Disney movie!\n")
     print(f"The name of this movie has {len(random_movie)} letters.\n")
-    
-    word_completion = 0
-    while word_completion != len(random_movie):
-        letter_guessed = input("Enter the letter you would like to guess:")
-        # It letter is in word, display letters completed and add count to workd completion
-
-        # If lettet is not there, prompt user to try agai
-
-
 
     print(random_movie)
     print(letter_list)
+    
+    word_completion = 0
+    guessed_letters = set()
 
+    while word_completion != word_length:
+        display_word = ""
+        for letter in random_movie:
+            if letter == " ":
+                display_word += " "
+
+            elif letter in guessed_letters:
+                display_word += letter
+            else:
+                display_word += "_"
+        print("Current word: ", display_word)
+            
+        print("Letters Used: ")   
+        print(guessed_letters)
+        guess = input("Enter the letter you would like to guess: ")       
+        letter_guessed = guess.lower()
+       
+
+        if letter_guessed in guessed_letters:
+            print("You already guessed that letter!\n")
+        
+        # It letter is in word, display letters completed and add count to word completion
+        if letter_guessed in letter_list:
+            print("Correct!")
+            guessed_letters.add(letter_guessed)
+            word_completion = word_completion + random_movie.count(letter_guessed)
+                     
+        # If letter is not there, prompt user to try again
+        else:
+            print("Try again!")
+            guessed_letters.add(letter_guessed)
+
+
+    print("Congratulations! You won!")
+    print(f"The movie name of the movie is {movie}!")
+    
 
 play_game()
